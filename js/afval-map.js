16cf65b6682a9c0c2d702ff753b7e9c9b4a18475
E-milioKa1 <emilio_ka1@hotmail.com>
2024-05-15 22:29:53 +0200

    Leaflet en Javascript
    - Map aan contact.html toegevoegd
    - Map aan afval.html toegevoegd
    - Code opgeschoond
    - Navbar in header gestoken ipv main

---
+++ js/afval-map.js
@@ -0,0 +1,19 @@
+var map = L.map('afvalMap').setView([51.17, 4.46], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Mortsel</a>'
+}).addTo(map);
+
+var circle = L.circle([51.17, 4.46], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+circle.bindPopup("Neona Smart City");
+
+
+var marker = L.marker([51.17, 4.46]).addTo(map);
+marker.bindPopup("Visit us here");
